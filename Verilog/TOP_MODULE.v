aa3534f01550db98009154927af17832e02de87d
Sagnik Bera <sagnikbera90@gmail.com>
2026-02-23 01:40:27 +0530

    Updated Top Module for pc

--- Verilog/TOP_MODULE.v
+++ Verilog/TOP_MODULE.v
@@ -1,9 +1,28 @@
+//PC ──► instr_mem ──► instr ──► opcode ──► control_unit ──► ALUControl ──► ALU ──► Result
+
 module cpu_top(
+    input clk,
+    input reset,
     input [15:0] A,B,
-    input [3:0] opcode,
     output [15:0] Result
 );
 wire [2:0] ALUControl;
+wire [15:0] pc_val;
+wire [15:0] instr;
+wire [3:0] opcode; 
+
+pc pc0(
+    .clk(clk),
+    .reset(reset),
+    .PC(pc_val)
+);
+
+instr_mem IM0 (
+    .addr(pc_val),
+    .instr(instr)
+);
+
+assign opcode= instr[15:12];
 
 control_unit CU(
     .opcode(opcode),
